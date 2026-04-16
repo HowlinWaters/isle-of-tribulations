f2ac648b81a84b4beefeb0de6ccdc772c59eea77
Alvin Lee <alvinleealvinleealvinlee@gmail.com>
2026-04-16 14:50:20 -0700

    Code cleanups and added missing audio

---
+++ Assets/Credits/CREDITS.md
@@ -0,0 +1,2 @@
+# Audio Sources
+Indoor Footsteps.wav - WakuWakuWakuWaku ([text](https://freesound.org/people/WakuWakuWakuWaku/sounds/366111/?page=3#comments))
--- Assets/Scripts/Girl/PlayMode.cs
+++ Assets/Scripts/Girl/PlayMode.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
 
 public class PlayMode : MonoBehaviour
@@ -10,11 +12,14 @@ public class PlayMode : MonoBehaviour
     [SerializeField] private float rotateSpeed = 120f;
     [SerializeField] private float jumpHeight = 1.2f;
     [SerializeField] private float gravityValue = -20f;
+    [SerializeField] private AudioClip footstepClip;
+    [SerializeField] private AudioSource audioSource;
 
     private Vector3 playerVelocity;
     private bool groundedPlayer;
     private Animator animator;
     private bool isAttacking;
+    
 
     void Start()
     {
@@ -24,11 +29,16 @@ public class PlayMode : MonoBehaviour
         }
 
         animator = activeChar.GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+
+        Debug.Log($"AudioSource: {audioSource}, Clip: {audioSource?.clip}, Volume: {audioSource?.volume}");
     }
 
     void Update()
     {
         groundedPlayer = controller.isGrounded;
+        bool isMoving = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S)
+                     || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D);
 
         if (groundedPlayer && playerVelocity.y < 0)
         {
@@ -38,14 +48,20 @@ public class PlayMode : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
+        Vector3 move = speed * vertical * transform.forward;
+
         transform.Rotate(0f, horizontal * rotateSpeed * Time.deltaTime, 0f);
 
-        Vector3 move = transform.forward * vertical * speed;
+        FootStepSound(isMoving);
 
         if (Input.GetKeyDown(KeyCode.Space) && groundedPlayer)
         {
-            playerVelocity.y = Mathf.Sqrt(jumpHeight * -2f * gravityValue);
-            animator.SetTrigger("Jump");
+            Jump();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return) && !isAttacking)
+        {
+            Attack();
         }
 
         playerVelocity.y += gravityValue * Time.deltaTime;
@@ -57,18 +73,37 @@ public class PlayMode : MonoBehaviour
 
         animator.SetFloat("Speed", Mathf.Abs(vertical));
         animator.SetBool("IsGrounded", controller.isGrounded);
+    
+    }
 
-
-        if (Input.GetKeyDown(KeyCode.Return) && !isAttacking)
-        {
+    void Attack()
+    {
         animator.SetTrigger("Attack");
         isAttacking = true;
         StartCoroutine(ResetAttack());
+    }
+
+    void Jump()
+    {
+        playerVelocity.y = Mathf.Sqrt(jumpHeight * -2f * gravityValue);
+        animator.SetTrigger("Jump");
+    }
+
+    void FootStepSound(bool isMoving)
+    {
+        if (isMoving && !audioSource.isPlaying)
+        {
+            audioSource.pitch = UnityEngine.Random.Range(0.9f, 1.1f);
+            audioSource.PlayOneShot(footstepClip);
         }
+        else if (!isMoving)
+        {
+            audioSource.Stop();
+        }
+    }
     IEnumerator ResetAttack()
     {
-        yield return new WaitForSeconds(0.61f); 
+        yield return new WaitForSeconds(0.61f);
         isAttacking = false;
-    }
     }
 }
