46edaf6c8de300441fcb9d153588435bc8fd21bd
Thel Nu Zaw <trixiethelnu@gmail.com>
2026-04-26 16:16:59 -0700

    fix some script

--- Assets/Scripts/Characters/Player/RockBreaker.cs
+++ Assets/Scripts/Characters/Player/RockBreaker.cs
@@ -2,99 +2,50 @@ using UnityEngine;
 
 public class RockBreaker : MonoBehaviour
 {
-    [SerializeField] private float range = 2f;
-    [SerializeField] private LayerMask RockLayer;
-    [SerializeField] private float rayHeight = 1f;
-
-    private Rock currentTarget;
-    private int enterStage = 0;
+    [SerializeField] private AudioSource hammerSound;
+    [SerializeField] private float hitRange = 5f;
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Return))
         {
-            HandleEnter();
+            HitClosestRock();
         }
     }
 
-    void HandleEnter()
+    void HitClosestRock()
     {
-        if (enterStage == 0)
-        {
-            SelectTarget();
-        }
-        else if (enterStage == 1)
-        {
-            ChargeTarget();
-        }
-        else if (enterStage == 2)
-        {
-            ExecuteBreak();
-        }
-    }
+        Rock[] allRocks = FindObjectsOfType<Rock>();
+        Rock closest = null;
+        float closestDist = hitRange;
 
-    void SelectTarget()
-    {
-        RaycastHit hit;
-        Vector3 origin = transform.position + Vector3.up * rayHeight;
-        Vector3 dir = transform.forward;
-        dir.y = 0f;
-        dir.Normalize();
-
-        if (Physics.Raycast(origin, dir, out hit, range, RockLayer))
+        foreach (Rock rock in allRocks)
         {
-            Rock ice = hit.collider.GetComponent<Rock>();
+            float dist = Vector3.Distance(transform.position, rock.transform.position);
 
-            if (ice != null)
+            if (dist < closestDist)
             {
-                currentTarget = ice;
-                enterStage = 1;
-                currentTarget.SetStage(1);
-                Debug.Log("Stage 1: Block selected");
+                closestDist = dist;
+                closest = rock;
             }
         }
-    }
 
-    void ChargeTarget()
-    {
-        if (currentTarget == null)
+        if (closest != null)
         {
-            ResetInteraction();
-            return;
+            if (hammerSound != null)
+            {
+                hammerSound.Play();
+            }
+
+            Vector3 dir = transform.forward;
+            dir.y = 0f;
+            dir = new Vector3(Mathf.Round(dir.x), 0f, Mathf.Round(dir.z)).normalized;
+
+            closest.Hit(dir);
         }
-
-        enterStage = 2;
-        currentTarget.SetStage(2);
-        Debug.Log("Stage 2: Block charged");
-    }
-
-    void ExecuteBreak()
-    {
-        if (currentTarget == null)
+        else
         {
-            ResetInteraction();
-            return;
+            Debug.Log("No rock found within range");
         }
-
-        Vector3 dir = transform.forward;
-        dir.y = 0f;
-        dir = new Vector3(Mathf.Round(dir.x), 0f, Mathf.Round(dir.z)).normalized;
-
-        currentTarget.SetStage(0);
-        currentTarget.Break(dir);
-
-        Debug.Log("Stage 3: Chain break executed");
-        ResetInteraction();
-    }
-
-    void ResetInteraction()
-    {
-        if (currentTarget != null)
-        {
-            currentTarget.SetStage(0);
-        }
-
-        currentTarget = null;
-        enterStage = 0;
     }
 }
