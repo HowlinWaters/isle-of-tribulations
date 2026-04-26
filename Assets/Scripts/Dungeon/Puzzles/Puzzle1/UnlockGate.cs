15d10b2896ca0941819955ee3678f655913dd8e3
Thel Nu Zaw <trixiethelnu@gmail.com>
2026-04-26 01:41:22 -0700

    add vfx and sound to gate open

--- Assets/Scripts/Dungeon/Puzzles/Puzzle1/UnlockGate.cs
+++ Assets/Scripts/Dungeon/Puzzles/Puzzle1/UnlockGate.cs
@@ -1,21 +1,56 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class UnlockGate : MonoBehaviour
 {
     [SerializeField] private int requiredkey = 1;
-    private void OnTriggerEnter(Collider c){
-        if(!c.CompareTag("Player")) return;
+    [SerializeField] private AudioSource gateopenSound;
+    [SerializeField] private GameObject gateopenVFX;
+
+    private bool unlocked = false;
+
+    private void OnTriggerEnter(Collider c)
+    {
+        if (unlocked) return;
+        if (!c.CompareTag("Player")) return;
 
         Inventory inventory = c.GetComponent<Inventory>();
 
-        if(inventory != null && inventory.HashKey(requiredkey)){
+        if (inventory != null && inventory.HashKey(requiredkey))
+        {
+            unlocked = true;
             inventory.UseKey(requiredkey);
-            Debug.Log("Unlock door");
-            gameObject.SetActive(false);
+
+            if (gateopenSound != null)
+                gateopenSound.Play();
+
+            if (gateopenVFX != null)
+            {
+                gateopenVFX.transform.SetParent(null);
+                gateopenVFX.SetActive(true);
+
+                ParticleSystem[] particles = gateopenVFX.GetComponentsInChildren<ParticleSystem>(true);
+
+                foreach (ParticleSystem ps in particles)
+                {
+                    ps.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+                    ps.Play();
+                }
+
+                Destroy(gateopenVFX, 2f);
+            }
+
+            Collider[] cols = GetComponentsInChildren<Collider>();
+            foreach (Collider col in cols)
+                col.enabled = false;
+
+            Renderer[] rends = GetComponentsInChildren<Renderer>();
+            foreach (Renderer r in rends)
+                r.enabled = false;
+
+            Destroy(gameObject, 2f);
         }
-        else{
+        else
+        {
             Debug.Log("Door is locked");
         }
     }
